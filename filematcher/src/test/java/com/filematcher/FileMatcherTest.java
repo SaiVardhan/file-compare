10b2234bba36518e2c800a489fc12cb285eeb2ca
Hanumakanth <hanumakanth@gmail.com>
2015-05-11 12:19:29 +0530

    modified tests

--- filematcher/src/test/java/com/filematcher/FileMatcherTest.java
+++ filematcher/src/test/java/com/filematcher/FileMatcherTest.java
@@ -52,7 +52,7 @@ public class FileMatcherTest {
 		List<PersonName> matchedList = fileMatcher.matchList(firstFileList,
 				secondFileList);
 
-		assertTrue(matchedList.size() == 1;
+		assertTrue(matchedList.size() == 1);
 	}
 
 	@Test
